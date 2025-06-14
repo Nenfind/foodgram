07e7db14a85119319ecb91971dc255a2d5b8b8cd
Di <modest.saz@gmail.com>
2025-06-14 16:26:17 +0300

    added explicit csrf handling, maybe admin panel will work

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -14,7 +14,7 @@ DEBUG = os.getenv('DJANGO_DEBUG') == 'True'
 
 ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
-CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+CSRF_TRUSTED_ORIGINS = ['https://nocatshere.ddns.net']
 
 CSRF_COOKIE_SECURE = True
 
