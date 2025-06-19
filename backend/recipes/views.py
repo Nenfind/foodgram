ba9b56a1d18d22ecbaf8849d7a43521d535e78cf
Di <modest.saz@gmail.com>
2025-06-19 08:39:11 +0300

    small fixes

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -6,6 +6,6 @@ from recipes.models import Recipe
 def follow_short_link(self, short_link=None):
     try:
         recipe = Recipe.objects.get(short_link__iexact=short_link)
-        return redirect(f'api/recipes/{recipe.pk}/')
+        return redirect(f'/recipes/{recipe.id}/')
     except Recipe.DoesNotExist:
         return redirect('/404/', permanent=False)
