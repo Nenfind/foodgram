c5dec37dfd2fe725630e00269698d00c7c517171
Di <modest.saz@gmail.com>
2025-06-19 08:27:41 +0300

    small fixes

--- backend/recipes/views.py
+++ backend/recipes/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 from recipes.models import Recipe
 
 
-def follow_short_link(self, request, short_link=None):
+def follow_short_link(self, short_link=None):
     try:
         recipe = Recipe.objects.get(short_link__iexact=short_link)
         return redirect(f'api/recipes/{recipe.pk}/')
